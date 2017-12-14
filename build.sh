a60a291df55fb949863e73d27795f3170f71bdb8
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-14 13:17:30 +0000

    Pull before build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 IMAGE=docker.montagu.dide.ic.ac.uk:5000/vimc-website-builder:i1205
-#docker pull $IMAGE
+docker pull $IMAGE
 VOLUME=$1
 BRANCH=$2
 if [ -z $VOLUME ]; then
